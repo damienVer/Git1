4a378feef75e9d34b63d65df14fc74e4837f6f7c
damien <damien.verschaere@gmail.com>
2020-06-26 16:23:30 +0200

    je test si on peut bien recuperer de work 3 vers work4

--- src/tets/Hello.java
+++ src/tets/Hello.java
@@ -3,7 +3,7 @@ package tets;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello le monde ");
+		System.out.println("hello le monde  la baise hamdoullah");
 
 	}
 
